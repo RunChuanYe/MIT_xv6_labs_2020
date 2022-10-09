693fa2c0444dffba9df0acb39969de7c6c1db918
yrc <908049707@qq.com>
2022-10-09 03:09:51 +0000

    task 1 done

--- user/uthread.c
+++ user/uthread.c
@@ -10,15 +10,36 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+typedef struct thread_context{
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+
+} thread_context;
+
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  thread_context context;  
 };
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(uint64 old, uint64 new);
               
 void 
 thread_init(void)
@@ -59,10 +80,11 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+
+    // this cause the main thread start to schedule then to run ,then exit(0)
+    // so comment it
+    // t->state = RUNNABLE;
+    thread_switch((uint64)(&t->context), (uint64)(&current_thread->context));
   } else
     next_thread = 0;
 }
@@ -76,7 +98,8 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)(t->stack + STACK_SIZE - 1);
 }
 
 void 
